5ce32e242d9f78d3abc76f30da468d37d21ad02c
Jasper Zonneveld <jasper@swis.nl>
2016-02-24 16:26:39 +0100

    Added locale for nl-nl

---
+++ src/js/i18n/datepicker.nl-nl.js
@@ -0,0 +1,11 @@
+Datepicker.language['nl-nl'] = {
+    days: ['zondag', 'maandag', 'dinsdag', 'woensdag', 'donderdag', 'vrijdag', 'zaterdag'],
+    daysShort: ['zo', 'ma', 'di', 'wo', 'do', 'vr', 'za'],
+    daysMin: ['zo', 'ma', 'di', 'wo', 'do', 'vr', 'za'],
+    months: ['Januari', 'Februari', 'Maart', 'April', 'Mei', 'Juni', 'Juli', 'Augustus', 'September', 'Oktober', 'November', 'December'],
+    monthsShort: ['Jan', 'Feb', 'Mrt', 'Apr', 'Mei', 'Jun', 'Jul', 'Aug', 'Sep', 'Okt', 'Nov', 'Dec'],
+    today: 'Vandaag',
+    clear: 'Legen',
+    dateFormat: 'dd-MM-yy',
+    firstDay: 0
+};
