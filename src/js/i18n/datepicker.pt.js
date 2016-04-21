fe45ba7eb1179b8c2e0aa91b495e48d0c0f8e794
t1m0n <t1m0n.TR@gmail.com>
2016-04-21 12:27:07 +0300

    rename 'pt-pt' to 'pt'

--- src/js/i18n/datepicker.pt.js
+++ src/js/i18n/datepicker.pt.js
@@ -1,12 +1,12 @@
-$.fn.datepicker.language['pt-BR'] = {
+$.fn.datepicker.language['pt'] = {
     days: ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'],
     daysShort: ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sab'],
-    daysMin: ['Do', 'Se', 'Te', 'Qu', 'Qu', 'Se', 'Sa'],
+    daysMin: ['Do', 'Se', 'Te', 'Qa', 'Qi', 'Sx', 'Sa'],
     months: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'],
     monthsShort: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'],
     today: 'Hoje',
     clear: 'Limpar',
     dateFormat: 'dd/mm/yyyy',
     timeFormat: 'hh:ii',
-    firstDay: 0
+    firstDay: 1
 };
