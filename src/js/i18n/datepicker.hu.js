5e442e467fe03afd9e94df5022cc4c63a32235ee
Szabó Gergő <gergo85@gmail.com>
2016-05-30 09:50:14 +0200

    Minor typo

--- src/js/i18n/datepicker.hu.js
+++ src/js/i18n/datepicker.hu.js
@@ -2,7 +2,7 @@
     days: ['Vasárnap', 'Hétfő', 'Kedd', 'Szerda', 'Csütörtök', 'Péntek', 'Szombat'],
     daysShort: ['Va', 'Hé', 'Ke', 'Sze', 'Cs', 'Pé', 'Szo'],
     daysMin: ['V', 'H', 'K', 'Sz', 'Cs', 'P', 'Sz'],
-    months: ['Január', 'Február', 'Március', 'Április', 'Május', 'Június', 'Július', 'Augusztus', 'Szeptember', 'Október', 'November','December'],
+    months: ['Január', 'Február', 'Március', 'Április', 'Május', 'Június', 'Július', 'Augusztus', 'Szeptember', 'Október', 'November', 'December'],
     monthsShort: ['Jan', 'Feb', 'Már', 'Ápr', 'Máj', 'Jún', 'Júl', 'Aug', 'Szep', 'Okt', 'Nov', 'Dec'],
     today: 'Ma',
     clear: 'Törlés',
