1d52fd03d5d218d3f20c30a22983bbae34ee6311
TomaszKs <xiio@users.noreply.github.com>
2016-06-01 16:04:59 +0200

    Fixed typo

--- src/js/i18n/datepicker.pl.js
+++ src/js/i18n/datepicker.pl.js
@@ -1,7 +1,7 @@
 $.fn.datepicker.language['pl'] = {
     days: ['Niedziela', 'Poniedziałek', 'Wtorek', 'Środa', 'Czwartek', 'Piątek', 'Sobota'],
     daysShort: ['Nie', 'Pon', 'Wto', 'Śro', 'Czw', 'Pią', 'Sob'],
-    daysMin: ['Nd', 'Pn', 'Wt', 'Śr', 'Czw', 'Pi', 'So'],
+    daysMin: ['Nd', 'Pn', 'Wt', 'Śr', 'Czw', 'Pt', 'So'],
     months: ['Styczeń','Luty','Marzec','Kwiecień','Maj','Czerwiec', 'Lipiec','Sierpień','Wrzesień','Październik','Listopad','Grudzień'],
     monthsShort: ['Sty', 'Lut', 'Mar', 'Kwi', 'Maj', 'Cze', 'Lip', 'Sie', 'Wrz', 'Paź', 'Lis', 'Gru'],
     today: 'Dzisiaj',
