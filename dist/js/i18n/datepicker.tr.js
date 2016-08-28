2ba5c79b9e3475a4d4d2a7d431b6fafcdcb68de9
dmarto <dmarto@hydrogenium>
2016-08-28 18:10:06 +0300

    Add Turkish language

---
+++ dist/js/i18n/datepicker.tr.js
@@ -0,0 +1,12 @@
+;(function ($) { $.fn.datepicker.language['tr'] = {
+   days: ['Pazar', 'Pazartesi', 'Sali', 'Carsamba', 'Persembe', 'Cuma', 'Cumartesi'],    
+   daysShort: ['Pzr','Pts', 'Sal', 'Crs', 'Prs', 'Cum','Cts'],    
+   daysMin: ['Pz', 'Pzt', 'Sl', 'Cr', 'Pr', 'Cm', 'Ct'],    
+   months: ['Ocak','Subat','Mart','Nisan','Mayis','Haziran', 'Temmuz','Agustos','Eylul','Ekim','Kasim','Aralik'],    
+   monthsShort: ['Ock', 'Sub', 'Mar', 'Nis', 'May', 'Hzr', 'Tmz', 'Aug', 'Eyl', 'Ekm', 'Ksm', 'Ara'],    
+   today: 'Bugun',    
+   clear: 'Sil',    
+   dateFormat: 'dd/mm/yyyy',
+   timeFormat: 'hh:ii',    
+   firstDay: 1
+}; })(jQuery);
