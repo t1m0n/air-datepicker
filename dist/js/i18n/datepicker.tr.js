a3be4b9e3fd4c0fcb737a0bcef82ee7de0f144d7
Ramazan APAYDIN <apaydin@MacBook-Pro.local>
2016-09-01 16:18:42 +0300

    Added Turkish language.

---
+++ dist/js/i18n/datepicker.tr.js
@@ -0,0 +1,12 @@
+;(function ($) { $.fn.datepicker.language['tr'] = {
+    days: ['Pazar', 'Pazartesi', 'Salı', 'Çarşamba', 'Perşembe', 'Cuma', 'Cumartesi'],
+    daysShort: ['Pzr', 'Pts', 'Sal', 'Çrş', 'Prş', 'Cum', 'Cts'],
+    daysMin: ['Pz', 'Pt', 'Sl', 'Çr', 'Pr', 'Cm', 'Ct'],
+    months: ['Ocak','Şubat','Mart','Nisan','Mayıs','Hazıran', 'Temmuz','Ağustos','Eylül','Ekim','Kasım','Aralık'],
+    monthsShort: ['Oca', 'Şub', 'Mar', 'Nis', 'May', 'Haz', 'Tem', 'Ağu', 'Eyl', 'Eki', 'Kas', 'Ara'],
+    today: 'Bugün',
+    clear: 'Temizle',
+    dateFormat: 'mm/dd/yyyy',
+    timeFormat: 'hh:ii aa',
+    firstDay: 0
+}; })(jQuery);
