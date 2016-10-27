e6aa2723fa63d2dafcd2dbaa1c87a5703d3800b4
BisonTech <evilbison@gmail.com>
2016-10-27 15:37:32 +0200

    Fix dateformat

--- src/js/i18n/datepicker.it.js
+++ src/js/i18n/datepicker.it.js
@@ -6,8 +6,8 @@
     monthsShort: ['Gen', 'Feb', 'Mar', 'Apr', 'Mag', 'Giu', 'Lug', 'Aug', 'Ago', 'Set', 'Nov', 'Dic'],
     today: 'Oggi',
     clear: 'Resetta',
-    dateFormat: 'dd/mm/yyyy',
-    timeFormat: 'HH:mm:ss',
+    dateFormat: 'dd-mm-yyyy',
+    timeFormat: 'hh:ii',
     language: 'it',
     firstDay: 1
 }; })(jQuery);
