005ba6a8effd7c56e14615d6cd48949b04d2e3d4
Ricky Riyadi <rickyriyadi@RCKRYD.local>
2019-11-04 12:00:21 +0700

    add indonesian language

---
+++ dist/js/i18n/datepicker.id.js
@@ -0,0 +1,14 @@
+; (function ($) {
+    $.fn.datepicker.language['id'] = {
+        days: ['Minggu', 'Senin', 'Selasa', 'Rabu', 'Kamis', 'Jum\'at', 'Sabtu'],
+        daysShort: ['Min', 'Sen', 'Sel', 'Rab', 'Kam', 'Jum', 'Sab'],
+        daysMin: ['Mg', 'Sn', 'Sl', 'Rb', 'Km', 'Jm', 'Sb'],
+        months: ['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember'],
+        monthsShort: ['Jan', 'Feb', 'Mar', 'Apr', 'Mei', 'Jun', 'Jul', 'Agu', 'Sep', 'Okt', 'Nov', 'Des'],
+        today: 'Hari ini',
+        clear: 'Hapus',
+        dateFormat: 'dd/mm/yyyy',
+        timeFormat: 'hh:ii aa',
+        firstDay: 0
+    };
+})(jQuery);
