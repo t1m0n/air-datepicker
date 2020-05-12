37bbc94d701c840152a7cdb02ae38cfb3428c340
t1m0n <t1m0n.TR@gmail.com>
2020-05-12 23:55:51 +0300

    create dp container and base html

--- src/datepicker.js
+++ src/datepicker.js
@@ -1,9 +1,28 @@
+/* eslint-disable */
 import defaults from './defaults';
-import {getEl} from './utils';
+import {getEl, createElement, insertAfter} from './utils';
 
 import './datepicker.scss';
 
-let $body = '';
+let $body = '',
+    $datepickersContainer = '',
+    containerBuilt = false,
+    baseTemplate = '' +
+        '<div class="datepicker">' +
+        '<i class="datepicker--pointer"></i>' +
+        '<nav class="datepicker--nav"></nav>' +
+        '<div class="datepicker--content"></div>' +
+        '</div>';
+
+function buildDatepickersContainer () {
+    containerBuilt = true;
+    let id = 'datepickers-container';
+
+    $datepickersContainer = createElement({className: id, id});
+    $body.appendChild($datepickersContainer);
+
+    return $datepickersContainer;
+}
 
 export default class AirDatepicker {
     constructor(el, opts) {
@@ -22,6 +41,49 @@ export default class AirDatepicker {
             this.elIsInput = true;
         }
 
+        this.inited = false;
+        this.visible = false;
+        this.silent = false; // Need to prevent unnecessary rendering
+
+        this.currentDate = this.opts.startDate;
+        this.currentView = this.opts.view;
+        this.selectedDates = [];
+        this.views = {};
+        this.keys = [];
+        this.minRange = '';
+        this.maxRange = '';
+        this._prevOnSelectValue = '';
+
+        this.init();
     }
+
+    init(){
+        if (!containerBuilt && !this.opts.inline && this.elIsInput) {
+            buildDatepickersContainer();
+        }
+        this._buildBaseHtml();
+    }
+
+    _buildBaseHtml() {
+        let $appendTarget,
+            $inline = createElement({className: 'datepicker-inline'});
+
+        if  (this.elIsInput) {
+            if (!this.opts.inline) {
+                $appendTarget = $datepickersContainer;
+            } else {
+                $appendTarget = insertAfter($inline, this.$el);
+            }
+        } else {
+            $appendTarget = this.$el.appendChild($inline);
+        }
+
+        $appendTarget.innerHTML = baseTemplate;
+
+        this.$datepicker = getEl('.datepicker', $appendTarget);
+        this.$content = getEl('.datepicker--content',  this.$datepicker);
+        this.$nav = getEl('.datepicker--nav', this.$datepicker);
+    }
+
     static defaults = defaults
 }
--- src/utils.js
+++ src/utils.js
@@ -1,9 +1,44 @@
 /**
  * Finds DOM element
  * @param {HTMLElement, String} el
+ * @param {Document|HTMLElement} [context=document]
  */
-export function getEl (el) {
+export function getEl (el, context=document) {
     return typeof el === 'string'
-        ? document.querySelector(el)
+        ? context['querySelector'](el)
         : el;
+}
+
+/**
+ * Creates HTML DOM element
+ * @param {String} tagName - element's tag name
+ * @param {String} className
+ * @param {String} id
+ * @param {Object} attrs
+ * @returns {HTMLElement}
+ */
+export function createElement({tagName='div', className='', id='', attrs={}} = {}) {
+    let $element = document.createElement(tagName);
+    if (className) $element.classList.add(className);
+    if (id) $element.id = id;
+
+    if (attrs) {
+        for (let attr in attrs) {
+            $element.setAttribute(attr, attrs[attr]);
+        }
+    }
+
+    return $element;
+}
+
+/**
+ * Inserts newElement after targetElement
+ * @param {HTMLElement} newElement - element to be inserted
+ * @param {HTMLElement} targetElement - after which must be inserted
+ * @return {HTMLElement} newElement
+ */
+export function insertAfter(newElement, targetElement) {
+    targetElement.parentNode
+        .insertBefore(newElement, targetElement.nextSibling);
+    return newElement;
 }
