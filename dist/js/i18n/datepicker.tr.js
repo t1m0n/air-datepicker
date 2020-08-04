22acc733ab38859852530cc0c8a75e682cb0f06b
Mustafa Zuvin <zuvin97@gmail.com>
2020-08-04 17:21:11 +0300

    added turkish language

---
+++ dist/js/i18n/datepicker.tr.js
@@ -0,0 +1,13 @@
+;(function ($) { $.fn.datepicker.language['tr'] = {
+  days: ['Pazar', 'Pazartesi', 'Salı', 'Çarşamba', 'Perşembe', 'Cuma', 'Cumartesi'],
+  daysShort: ['Pzr', 'Pzt', 'Sal', 'Çar', 'Per', 'Cum', 'Cmt'],
+  daysMin: ['Pz', 'Pt', 'Sa', 'Ça', 'Pe', 'Cu', 'Ct'],
+  months: ['Ocak','Şubat','Mart','Nisan','Mayıs','Haziran', 'Temmuz','Ağustos','Eylül','Ekim','Kasım','Aralık'],
+  monthsShort: ['Oca', 'Şub', 'Mar', 'Nis', 'May', 'Haz', 'Tem', 'Ağu', 'Eyl', 'Eki', 'Kas', 'Ara'],
+  today: 'Bugün',
+  clear: 'Temizle',
+  dateFormat: 'dd.mm.yyyy',
+  timeFormat: 'hh:ii',
+  firstDay: 1
+};
+})(jQuery);
