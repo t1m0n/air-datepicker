ae416d794d9ed380f1948e1b9028ecdad1983b75
t1m0n <t1m0n.TR@gmail.com>
2021-10-01 12:51:44 +0300

    begin types for locales

--- scripts/build.js
+++ scripts/build.js
@@ -9,6 +9,7 @@ const readFile = util.promisify(fs.readFile);
 const writeFile = util.promisify(fs.writeFile);
 const rimraf = require('rimraf');
 const copyPackageFiles = require('./copyPackageFiles');
+const generateLocaleTypes = require('./generateLocaleTypes');
 
 let babelConfig = path.resolve(__dirname, '../babel.config.js');
 let srcPath = path.resolve('./src');
@@ -27,40 +28,46 @@ let log = {
 };
 
 async function run() {
-    log.info('Build started');
-
-    rimraf.sync(distPath);
-    fs.mkdirSync(distPath);
-
     try {
-        await execAsync('set NODE_ENV="production"&& webpack');
-        log.success('Bundle compiled successfully');
+        await generateLocaleTypes();
     } catch (e) {
-        log.error(`Bundle compilation error: ${e}`);
+        log.error('Error while generating types for locale files', e);
     }
 
-    try {
-        await execAsync(`babel ${srcPath}/locale --out-dir ${distPath}/locale --config-file ${babelConfig}`);
-        log.success('Localization files compiled');
-    } catch (e) {
-        log.error('Error, while handling localization files');
-    }
-
-    try {
-        await copyPackageFiles();
-        log.success('Package files are copied');
-    } catch (e) {
-        log.error(`Error, while copying package files: ${e}`);
-    }
-
-    try {
-        await writeFile(
-            path.join(distPath, 'index.es.js'), 
-            'import AirDatepicker from \'./air-datepicker\';\nexport default AirDatepicker'
-        );
-    } catch (e) {
-        log.error('Could not create ES index file');
-    }
+    // log.info('Build started');
+    //
+    // rimraf.sync(distPath);
+    // fs.mkdirSync(distPath);
+    //
+    // try {
+    //     await execAsync('set NODE_ENV="production"&& webpack');
+    //     log.success('Bundle compiled successfully');
+    // } catch (e) {
+    //     log.error(`Bundle compilation error: ${e}`);
+    // }
+    //
+    // try {
+    //     await execAsync(`babel ${srcPath}/locale --out-dir ${distPath}/locale --config-file ${babelConfig}`);
+    //     log.success('Localization files compiled');
+    // } catch (e) {
+    //     log.error('Error, while handling localization files');
+    // }
+    //
+    // try {
+    //     await copyPackageFiles();
+    //     log.success('Package files are copied');
+    // } catch (e) {
+    //     log.error(`Error, while copying package files: ${e}`);
+    // }
+    //
+    // try {
+    //     await writeFile(
+    //         path.join(distPath, 'index.es.js'),
+    //         'import AirDatepicker from \'./air-datepicker\';\nexport default AirDatepicker'
+    //     );
+    // } catch (e) {
+    //     log.error('Could not create ES index file');
+    // }
 }
 
 run();
