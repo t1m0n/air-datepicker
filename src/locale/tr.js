fd4015730c529d768cb6b6a3a72aaeb438239b18
Özkan Özcan <grafikerozkan@gmail.com>
2021-10-30 02:06:43 +0300

    Turkish Translation completed

---
+++ src/locale/tr.js
@@ -0,0 +1,14 @@
+export default {
+    days: ['Pazar', 'Pazartesi', 'Salı', 'Çarşamba', 'Perşembe', 'Cuma', 'Cumartesi'],
+    daysShort: ['Pzr', 'Pts', 'Sl', 'Çar', 'Per', 'Cum', 'Cts'],
+    daysMin: ['Pa', 'Pt', 'Sl', 'Ça', 'Pe', 'Cu', 'Ct'],
+    months: ['Ocak', 'Şubat', 'Mart', 'Nisan', 'Mayıs', 'Haziran', 'Temmuz', 'Ağustos', 'Eylül', 'Ekim', 'Kasım', 'Aralık'],
+    monthsShort: ['Oca', 'Şbt', 'Mrt', 'Nsn', 'Mys', 'Hzr', 'Tmz', 'Ağt', 'Eyl', 'Ekm', 'Ksm', 'Arl'],
+    today: 'Bugün',
+    clear: 'Temizle',
+    dateFormat: 'dd.MM.yyyy',
+    timeFormat: 'hh:mm aa',
+    firstDay: 1
+};
+
+
