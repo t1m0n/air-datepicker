023e36fb1c2a15812bd71c3ca827bca9c2fe84f4
Anton Andreasson Gistedt <anton@andreasson.org>
2022-05-18 23:53:56 +0200

    Swedish localization (#207)
    * Create datepicker.sv.js
    
    Swedish localization
    
    * Use new date/time format

---
+++ src/locale/sv.js
@@ -0,0 +1,12 @@
+export default {
+    days: ['Söndag', 'Måndag', 'Tisdag', 'Onsdag', 'Torsdag', 'Fredag', 'Lördag'],
+    daysShort: ['Sön', 'Mån', 'Tis', 'Ons', 'Tor', 'Fre', 'Lör'],
+    daysMin: ['Sö', 'Må', 'Ti', 'On', 'To', 'Fr', 'Lö'],
+    months: ['Januari', 'Februari', 'Mars', 'April', 'Maj', 'Juni', 'Juli', 'Augusti', 'September', 'Oktober', 'November', 'December'],
+    monthsShort: ['Jan', 'Feb', 'Mar', 'Apr', 'Maj', 'Jun', 'Jul', 'Aug', 'Sep', 'Okt', 'Nov', 'Dec'],
+    today: 'I dag',
+    clear: 'Nollställ',
+    dateFormat: 'yyyy-MM-dd',
+    timeFormat: 'HH:mm',
+    firstDay: 1
+};
