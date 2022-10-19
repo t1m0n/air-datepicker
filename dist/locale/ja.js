0410cef352ad19468339e43a3bfea69246231fc3
kimkiyong <fenderst0612@gmail.com>
2022-10-19 18:46:45 +0900

    feat: locate ja

---
+++ dist/locale/ja.js
@@ -0,0 +1,19 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+var _default = {
+  days: ['日', '月', '火', '水', '木', '金', '土'],
+  daysShort: ['日', '月', '火', '水', '木', '金', '土'],
+  daysMin: ['日', '月', '火', '水', '木', '金', '土'],
+  months: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月'],
+  monthsShort: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月'],
+  today: '今日',
+  clear: 'クリア',
+  dateFormat: 'yyyy/MM/dd',
+  timeFormat: 'hh:ii',
+  firstDay: 0
+};
+exports.default = _default;
