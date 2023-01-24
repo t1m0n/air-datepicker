9b8a0bac5140eb0b745d8d2b1ced7059c8d38280
t1m0n <t1m0n.TR@gmail.com>
2023-01-24 22:57:42 +0300

    updated packages, added `proposal-optional-chaining` plugin, fixes #518

---
+++ dist/locale/id.js
@@ -0,0 +1,19 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+var _default = {
+  days: ['Minggu', 'Senin', 'Selasa', 'Rabu', 'Kamis', 'Jumat', 'Sabtu'],
+  daysShort: ['Min', 'Sen', 'Sel', 'Rab', 'Kam', 'Jum', 'Sab'],
+  daysMin: ['Min', 'Sen', 'Sel', 'Rab', 'Kam', 'Jum', 'Sab'],
+  months: ['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember'],
+  monthsShort: ['Jan', 'Feb', 'Mar', 'Apr', 'Mei', 'Jun', 'Jul', 'Agt', 'Sep', 'Okt', 'Nov', 'Des'],
+  today: 'Hari ini',
+  clear: 'Hapus',
+  dateFormat: 'dd/MM/yyyy',
+  timeFormat: 'hh:mm aa',
+  firstDay: 1
+};
+exports.default = _default;
