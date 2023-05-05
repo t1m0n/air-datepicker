09ff82c9ae9a8972b9266b5259cc355a1b3abe47
José Alberto Torrents Batista <jtorrents@re7systems.com>
2023-05-05 16:56:16 +0200

    Added Catalan localization

---
+++ src/locale/ca.js
@@ -0,0 +1,12 @@
+export default {
+    days: ["Diumenge", "Dilluns", "Dimarts", "Dimecres", "Dijous", "Divendres", "Dissabte"],
+    daysShort: ["Diu",  "Dil", "Dmt", "Dmc", "Dij", "Div", "Dis"],
+    daysMin: ["dg", "dl", "dt", "dc", "dj", "dv", "ds"],
+    months: ["Gener", "Febrer", "Març", "Abril", "Maig", "Juny", "Juliol", "Agost", "Setembre", "Octubre", "Novembre", "Desembre"],
+    monthsShort: ["Gen", "Feb", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Oct", "Nov", "Des"],
+    today: "Avui",
+    clear: 'Neteja',
+    dateFormat: 'dd/MM/yyyy',
+    timeFormat: 'hh:mm aa',
+    firstDay: 1
+};
