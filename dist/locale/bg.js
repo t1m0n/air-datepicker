c0632239c8f87338935a9a524393387987bac86e
t1m0n <t1m0n.TR@gmail.com>
2023-07-25 00:01:47 +0300

    added new localizations to the `dist`

---
+++ dist/locale/bg.js
@@ -0,0 +1,19 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+var _default = {
+  days: ["Неделя", "Понеделник", "Вторник", "Сряда", "Четвъртък", "Петък", "Събота"],
+  daysShort: ["Нед", "Пон", "Вто", "Сря", "Чет", "Пет", "Съб"],
+  daysMin: ["Нд", "Пн", "Вт", "Ср", "Чт", "Пт", "Сб"],
+  months: ["Януари", "Февруари", "Март", "Април", "Май", "Юни", "Юли", "Август", "Септември", "Октомври", "Ноември", "Декември"],
+  monthsShort: ["Яну", "Фев", "Мар", "Апр", "Май", "Юни", "Юли", "Авг", "Сеп", "Окт", "Ное", "Дек"],
+  today: "Днес",
+  clear: "Изчисти",
+  dateFormat: "dd.MM.yyyy",
+  timeFormat: 'HH:mm',
+  firstDay: 1
+};
+exports.default = _default;
---
+++ dist/locale/hr.d.ts
@@ -0,0 +1,6 @@
+declare module 'air-datepicker/locale/hr' {
+    import {AirDatepickerLocale} from 'air-datepicker';
+    const hr: AirDatepickerLocale;
+
+    export default hr;
+}
