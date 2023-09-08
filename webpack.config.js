cd4e10954d6bfcf2382c825840dbce081ffe7ba2
Ali Fazelzadeh <afzcode@gmail.com>
2023-09-08 07:20:51 +0330

    trying another solution

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ const NAME = 'air-datepicker';
 let plugins = [
     new webpack.DefinePlugin({
         'process.env': {
-            'NODE_ENV': (JSON.stringify(process.env.NODE_ENV) || '').toLowerCase()
+            'NODE_ENV': typeof process.env.NODE_ENV === 'string' ? JSON.stringify(process.env.NODE_ENV).toLowerCase() : ''
         }
     }),
     new HtmlWebpackPlugin({
