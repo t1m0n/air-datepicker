9aeeb928f2adc3bb6dfee9cebcb8879af641c6c9
Morten Spjøtvoll <70020680+MortenSpjotvoll@users.noreply.github.com>
2024-03-05 21:30:15 +0100

    Added Norwegian localization (#521)
    * Added Norwegian localization

---
+++ src/locale/nb.js
@@ -0,0 +1,12 @@
+export default {
+    days: ["Søndag", "Mandag", "Tirsdag", "Onsdag", "Torsdag", "Fredag", "Lørdag"],
+    daysShort: ["Søn", "Man", "Tir", "Ons", "Tor", "Fre", "Lør"],
+    daysMin: ["Sø", "Ma", "Ti", "On", "To", "Fr", "Lø"],
+    months: ["Januar", "Februar", "Mars", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Desember"],
+    monthsShort: ["Jan", "Feb", "Mar", "Apr", "Mai", "Jun", "Jul", "Aug", "Sep", "Okt", "Nov", "Des"],
+    today: "Idag",
+    clear: "Fjern",
+    dateFormat: "dd.mm.yyyy",
+    timeFormat: "HH:mm",
+    firstDay: 1
+};
