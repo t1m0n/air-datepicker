28cb39a4348362480368d552884f8b5f30e485af
Madis E <madis@22systems.ee>
2024-07-23 19:01:29 +0300

    Fixed Estonian locale

---
+++ src/locale/et.js
@@ -0,0 +1,12 @@
+export default {
+    days: ['Pühapäev', 'Esmaspäev', 'Teisipäev', 'Kolmapäev', 'Neljapäev', 'Reede', 'Laupäev'],
+    daysShort: ['Pühap', 'Esmasp', 'Teisip', 'Kolmap', 'Neljap', 'Reede', 'Laup'],
+    daysMin: ['P', 'E', 'T', 'K', 'N', 'R', 'L'],
+    months: ['Jaanuar', 'Veebruar', 'Märts', 'Aprill', 'Mai', 'Juuni', 'Juuli', 'August', 'September', 'Oktoober', 'November', 'Detsember'],
+    monthsShort: ['Jaan', 'Veebr', 'Marts', 'Apr', 'Mai', 'Juuni', 'Juuli', 'Aug', 'Sept', 'Okt', 'Nov', 'Dets'],
+    today: 'Täna',
+    clear: 'Puhasta',
+    dateFormat: 'dd.MM.yyyy',
+    timeFormat: 'HH:mm',
+    firstDay: 1
+};
